6def2bef3a4a36410a73fcb7dd789e8273356ba3
mnyak <mnyakk87@gmail.com>
2022-10-31 23:07:56 +0900

    いったん退避

--- app/src/pokeapi.ts
+++ app/src/pokeapi.ts
@@ -18,44 +18,9 @@ export class Poke {
 	public static apiUrl = `/api/v2/pokemon/`
 	static async get(num: number) {
 		console.log(num)
-		// const tempPokeArr = [
-		// 	{
-		// 		id: 36,
-		// 		name: 'clefable',
-		// 		url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/36.png',
-		// 		text: '１キロさきで　おちた　ハリの　おとも\nききわける　すぐれた　みみを　もつ。\nしずかな　やまおくに　すんでいる。',
-		// 	},
-		// 	{
-		// 		id: 16,
-		// 		name: 'pidgey',
-		// 		url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png',
-		// 		text: 'もりや　はやしに　おおく　ぶんぷ。\nちじょうでも　はげしく　はばたいて\nすなを　かけたりする。',
-		// 	},
-		// 	{
-		// 		id: 6,
-		// 		name: 'charizard',
-		// 		url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
-		// 		text: 'くちから　しゃくねつの　ほのおを\nはきだすとき　しっぽの　さきは\nより　あかく　はげしく　もえあがる。',
-		// 	},
-		// 	{
-		// 		id: 40,
-		// 		name: 'wigglytuff',
-		// 		url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/40.png',
-		// 		text: '２ひき　よりそいあうと　おたがいの\nけがわが　きもちよすぎて\nはなれられなく　なってしまう。',
-		// 	},
-		// 	{
-		// 		id: 40,
-		// 		name: 'wigglytuff',
-		// 		url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/40.png',
-		// 		text: '２ひき　よりそいあうと　おたがいの\nけがわが　きもちよすぎて\nはなれられなく　なってしまう。',
-		// 	},
-		// ]
-		// return tempPokeArr
-		/////////
-
 		const pokeArr = this.createPokeArr(num)
 
-		return new Promise((resolve, reject) => {
+		new Promise((resolve, reject) => {
 			P.getResource(pokeArr)
 				.then(async (pokes) => {
 					const _pokeObjArr: PokeArrayObj[] = []
